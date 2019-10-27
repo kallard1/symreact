0856a60f8541553122080a310c5aa0e2e09b5a67
kallard <contact@allard-kevin.fr>
2019-10-27 12:05:08 +0100

    feat: moving namespace + renaming file

--- src/EventSubscriber/UserSubscriber.php
+++ src/EventSubscriber/UserSubscriber.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace App\Events;
+namespace App\EventSubscriber;
 
 use ApiPlatform\Core\EventListener\EventPriorities;
 use App\Entity\User;
@@ -11,7 +11,7 @@ use Symfony\Component\HttpKernel\Event\ViewEvent;
 use Symfony\Component\HttpKernel\KernelEvents;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
-class PasswordEncoderSubscriber implements EventSubscriberInterface
+class UserSubscriber implements EventSubscriberInterface
 {
     /**
      * @var UserPasswordEncoderInterface $encoder
@@ -19,7 +19,7 @@ class PasswordEncoderSubscriber implements EventSubscriberInterface
     private $encoder;
 
     /**
-     * PasswordEncoderSubscriber constructor.
+     * UserSubscriber constructor.
      *
      * @param UserPasswordEncoderInterface $encoder
      */
@@ -32,20 +32,7 @@ class PasswordEncoderSubscriber implements EventSubscriberInterface
     /**
      * Returns an array of event names this subscriber wants to listen to.
      *
-     * The array keys are event names and the value can be:
-     *
-     *  * The method name to call (priority defaults to 0)
-     *  * An array composed of the method name to call and the priority
-     *  * An array of arrays composed of the method names to call and respective
-     *    priorities, or 0 if unset
-     *
-     * For instance:
-     *
-     *  * ['eventName' => 'methodName']
-     *  * ['eventName' => ['methodName', $priority]]
-     *  * ['eventName' => [['methodName1', $priority], ['methodName2']]]
-     *
-     * @return array The event names to listen to
+     * @return mixed[] The event names to listen to
      */
     public static function getSubscribedEvents(): array
     {
